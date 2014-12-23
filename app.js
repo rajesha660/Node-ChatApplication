d1b62923bfdafaf383e5952d8f91795a895338fd
rajesha <rajesh.ayyavu@object-frontier.com>
2014-12-23 17:02:57 +0530

    Node js chat application

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express')
+	, app = express()
+	, http = require('http').Server(app)
+	, io = require('socket.io')(http)
+	, bodyParser = require('body-parser')
+	, cons = require('consolidate');
+
+var messages = {};
+
+app.engine('html', cons.swig)
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views')
+
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.render('index', {});
+});
+
+app.post('/chat', function(req, res) {
+	var username = req.body.username;
+	res.cookie('username', username);
+	res.render('chat', {'username': username});
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function(data){
+
+		io.emit('chat message', data);
   });
 });
 
